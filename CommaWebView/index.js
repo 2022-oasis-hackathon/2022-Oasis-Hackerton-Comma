887cab5b26ce8ffef1492405c00371569b780eed
loopy-lim <loopy@jnu.ac.kr>
2022-07-18 20:25:58 +0900

    fix: error paths

--- CommaWebView/index.js
+++ CommaWebView/index.js
@@ -3,10 +3,6 @@ const app = express();
 const path = require("path");
 app.use("/dist", express.static(__dirname + "/dist"));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/index.html"));
-});
-
 app.listen(3000, () => {
   console.log("server open");
 });
