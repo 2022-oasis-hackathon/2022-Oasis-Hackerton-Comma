1ca69aff069939a308caf002effb48cd3df88ca5
loopy-lim <loopy@jnu.ac.kr>
2022-07-19 08:18:44 +0900

    feat: add home page

---
+++ CommaWebView/dist/js/home.js
@@ -0,0 +1,21 @@
+$("document").ready(function () {
+  var MainSwiper = new Swiper(".main-swiper .swiper-container", {
+    slidesPerView: 1,
+    centeredSlides: true,
+  });
+  var SubSwiper = new Swiper(".sub-swiper .swiper-container", {
+    slidesPerView: 1,
+    centeredSlides: true,
+  });
+  MainSwiper.controller.control = SubSwiper;
+  SubSwiper.controller.control = MainSwiper;
+
+  var CitySwiper = new Swiper(".mySwiper", {
+    spaceBetween: 20,
+    slidesPerView: 5,
+    loop: false,
+    freeMode: false,
+    slidesOffsetBefore: 20,
+    slidesOffsetAfter: 20,
+  });
+});
--- CommaWebView/dist/js/wando.js
+++ CommaWebView/dist/js/wando.js
@@ -1,5 +1,6 @@
 new fullpage("#fullpage", {
   //options here
+  licenseKey: "gplv3-license",
   autoScrolling: true,
   scrollHorizontally: true,
 });
