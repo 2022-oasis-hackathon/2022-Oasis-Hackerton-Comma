b77b7e7b6fd47d76d5b332eba4602b6cec2a01c8
myungsun <audtjs9987@naver.com>
2022-07-20 00:05:21 +0900

    feat: add backButton and titleLabel in FullWebViewVC

--- Comma/ViewControllers/JourneyVC.swift
+++ Comma/ViewControllers/JourneyVC.swift
@@ -10,9 +10,14 @@ import WebKit
 
 
 class JourneyVC: UIViewController {
-
-
+    // UIView
     @IBOutlet weak var journeyView: UIView!
+    
+    // UIButton
+    @IBOutlet weak var backButton: UIButton!
+    
+    // UILbael
+    @IBOutlet weak var titleLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +27,11 @@ class JourneyVC: UIViewController {
         webViewManager.configureWebView(isTopView: false, isBottomView: true)
         webViewManager.getWebView(view: journeyView, viewcontoller: self ,url: "http://127.0.0.1:5500/CommaWebView/dist/html/home.html")
     }
+    
+    @IBAction func tapBackButton(_ sender: UIButton) {
+        // TODO: 뒤로가기 버튼 이벤트 처리
+    }
+    
 }
 
 extension JourneyVC: WKScriptMessageHandler {
@@ -32,5 +42,11 @@ extension JourneyVC: WKScriptMessageHandler {
             webViewManager.getWebView(view: UIView(), viewcontoller: self, url: "http://127.0.0.1:5500/CommaWebView/dist/html/wando_detail.html")
             
         }
+        
+        if let body = message.body as? String, body == "wando" {
+            self.titleLabel.alpha = 1
+            self.backButton.alpha = 1
+        }
+        
     }
 }
