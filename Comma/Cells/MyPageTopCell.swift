a5bda43bc668e598fd66dfa0af36f2b777a2e7b0
myungsun <audtjs9987@naver.com>
2022-07-19 14:20:31 +0900

    feat: develop mypage

--- Comma/Cells/MyPageTopCell.swift
+++ Comma/Cells/MyPageTopCell.swift
@@ -8,10 +8,18 @@
 import UIKit
 
 class MyPageTopCell: UITableViewCell {
-
+    // UILabel
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    // Constants
+    let TITLE_FONT_SIZE: CGFloat = 24
+    let DECRIPTION_FONT_SIZE: CGFloat = 20
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        titleLabel.font = FontManager.shared.getAppleSDGothicNeoBold(fontSize: TITLE_FONT_SIZE)
+        descriptionLabel.font = FontManager.shared.getAppleSDGothicNeoMedium(fontSize: DECRIPTION_FONT_SIZE)
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
--- Comma/ViewControllers/MyPageVC.swift
+++ Comma/ViewControllers/MyPageVC.swift
@@ -11,18 +11,70 @@ class MyPageVC: UIViewController {
     // UITableView
     @IBOutlet weak var myPageTableView: UITableView!
     
+    // Variables
+    var myPageList = [
+        [""],
+        ["나의 프로필",
+        "나의 Passport",
+        "내가 담은 공간",
+        "쿠폰함",
+        "환경설정",
+        "자주 묻는 질문",
+        "Comma 서비스 소개",
+        "문의하기"]
     
+    ]
+    
+    // Constants
+    let SECTION_NUNBER = 2
     override func viewDidLoad() {
         super.viewDidLoad()
         initUI()
     }
     
     private func initUI() {
-        
+        // UITableView
+        configureTableView()
     }
     
     private func configureTableView() {
-        
+        myPageTableView.delegate = self
+        myPageTableView.dataSource = self
+        myPageTableView.register(UINib(nibName: "MyPageTopCell", bundle: nil), forCellReuseIdentifier: "MyPageTopCell")
+        myPageTableView.register(UINib(nibName: "MyPageBottomCell", bundle: nil), forCellReuseIdentifier: "MyPageBottomCell")
     }
 
+}
+
+extension MyPageVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myPageList[section].count
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return SECTION_NUNBER
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "MyPageTopCell", for: indexPath) as! MyPageTopCell
+            cell.titleLabel.text = "Hello, Eddy님 !"
+            cell.descriptionLabel.text = "Life is Short"
+            return cell
+        }
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyPageBottomCell", for: indexPath) as! MyPageBottomCell
+        
+        cell.titleLabel.text = myPageList[indexPath.section][indexPath.row]
+        if indexPath.section == 1 && (indexPath.row == 0 || indexPath.row == 5) {
+            cell.lineView.isHidden = false
+            cell.lineView.alpha = 1
+        } else {
+            cell.lineView.isHidden = true
+        }
+        
+        cell.selectionStyle = .none
+        return cell
+    }
+    
+    
 }
