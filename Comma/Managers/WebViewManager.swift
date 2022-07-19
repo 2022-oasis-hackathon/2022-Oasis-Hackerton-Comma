955136a9ebe9fc84a9bd6ea0209146361ba8f2f1
myungsun <audtjs9987@naver.com>
2022-07-19 21:34:01 +0900

    feat: develop my passport

--- Comma/Managers/WebViewManager.swift
+++ Comma/Managers/WebViewManager.swift
@@ -10,26 +10,26 @@ import UIKit
 import WebKit
 
 class WebViewManager {
-    static let shared = WebViewManager()
-    var webView: WKWebView
-    var isTopView: Bool
-    var isBottomView: Bool
-    
-    private init() {}
-    
-    func configureWebView(isTopView: Bool, isBottomView: Bool) {
-        
-    }
-    
-    func getWebView(view: UIView, url: String) {
-        let webConfiguration = WKWebViewConfiguration()
-        webConfiguration.allowsInlineMediaPlayback = true
-                webConfiguration.mediaTypesRequiringUserActionForPlayback = []
-        webConfiguration.preferences.javaScriptEnabled = true
-        let url = URL(string: url)
-        let request = URLRequest(url: url!)
-        webView = WKWebView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height), configuration: webConfiguration)
-        view.addSubview(webView)
-        webView.load(request)
-    }
+//    static let shared = WebViewManager()
+////    var webView: WKWebView
+////    var isTopView: Bool
+////    var isBottomView: Bool
+//
+//    private init() {}
+//
+//    func configureWebView(isTopView: Bool, isBottomView: Bool) {
+//
+//    }
+//
+//    func getWebView(view: UIView, url: String) {
+//        let webConfiguration = WKWebViewConfiguration()
+//        webConfiguration.allowsInlineMediaPlayback = true
+//                webConfiguration.mediaTypesRequiringUserActionForPlayback = []
+//        webConfiguration.preferences.javaScriptEnabled = true
+//        let url = URL(string: url)
+//        let request = URLRequest(url: url!)
+//        webView = WKWebView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height), configuration: webConfiguration)
+//        view.addSubview(webView)
+//        webView.load(request)
+//    }
 }
