4b70c1ea39bfaa4d8c16e6729adbbf3c58b3ed75
myungsun <audtjs9987@naver.com>
2022-07-19 14:26:43 +0900

    feat: add MyProfileVC

---
+++ Comma/ViewControllers/MyProfileVC.swift
@@ -0,0 +1,15 @@
+//
+//  MyProfileVC.swift
+//  Comma
+//
+//  Created by myungsun on 2022/07/19.
+//
+
+import UIKit
+
+class MyProfileVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
