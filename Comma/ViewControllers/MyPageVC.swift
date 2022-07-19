40b6f6ce94100fe306ced02f7fb1cf2e025fcb5a
myungsun <audtjs9987@naver.com>
2022-07-19 13:48:00 +0900

    feat: add MyPageVC

---
+++ Comma/ViewControllers/MyPageVC.swift
@@ -0,0 +1,19 @@
+//
+//  MyPageVC.swift
+//  Comma
+//
+//  Created by myungsun on 2022/07/19.
+//
+
+import UIKit
+
+class MyPageVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
